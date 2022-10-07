559dda4d2ca8b1890fcdaa1833dfd09fede7ffed
Melanie Kazenel <melanie.kazenel@gmail.com>
2022-10-07 10:37:21 -0400

    second commit

--- BeeSexRatiosCode.R
+++ BeeSexRatiosCode.R
@@ -1,5 +1,15 @@
 # Bee sex ratios and climate change
+# RMBL bee and plant phenology data project
 # Melanie Kazenel
+# Created 7 October 2022
+# ---------------------------------
+
+# load relevant libraries
+library(dplyr)
+library(tidyverse)
+library(effects)
+library(lme4)
+library(dplyr)
 
 # set wd
 setwd("~/Documents/*RMBLPhenology/Projects/BeeSexRatios/Analyses")
